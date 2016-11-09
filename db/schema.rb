1f35586064294d8d2078ef9653ab0f8e65f2c05f
Mihai <mihai.iftode94@gmail.com>
2016-11-09 12:39:27 +0200

    Added admin_user method, fixed tests, fixed default db admin field

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 20161109092119) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
   end
 
 end
