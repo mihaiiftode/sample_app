88727733659f1a22a9970856092fa3f2e82834cb
Mihai <mihai.iftode94@gmail.com>
2016-11-09 11:17:34 +0200

    Add users page with pagination

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
-  test "layout links" do
+  test "layout links for non-logged users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -14,4 +17,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  test "layout links for logged users" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+
+
 end
