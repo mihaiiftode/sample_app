9bca528b718626f82344ed4b251ea0ca1b1c580e
Mihai <mihai.iftode94@gmail.com>
2016-11-09 11:26:23 +0200

    Add administrative users and tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,4 +44,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: { password:              'abcabc',
+                password_confirmation: 'abcabc',
+                admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
