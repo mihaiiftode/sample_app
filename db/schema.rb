ec4c88a75f66a4cabaf8ec900b88f19d3e6c60c0
Mihai <mihai.iftode94@gmail.com>
2016-11-10 12:15:05 +0200

    Add password reset controller and page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110084529) do
+ActiveRecord::Schema.define(version: 20161110101027) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20161110084529) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
   end
 
 end
